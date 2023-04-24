8c9773e6155fc0c2d6265ae33686c1636df905e7
Nexusrex <ahmedtahoun55@gmail.com>
2023-04-24 20:04:54 +0200

    add helper functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<conio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "linkedlist.h"
@@ -39,29 +40,111 @@ void readName(char *name){
     }while(str[0] == '\n');
 }
 
+void readClass(int *classNum)
+{
+    //Only allows digits between 1 and 9
+    char c;
+    while (1)
+    {
+        c = getchar();
+        if (c >= '1' && c <= '9')
+        {
+            *classNum = c - '0';
+            return;
+        }
+    }
+    return;
+}
+
+int readID(int *ID)
+{
+    char text[7];
+    int output = 0;
+    do {
+        fgets(text, sizeof(text), stdin);
+    } while (text[0] == '\n');
+
+    int i = 0;
+    for(i = 0; i<strlen(text); i++) {
+        if (text[i] >= '0' && text[i] <= '9')
+            output = (output * 10) + (text[i]-'0');
+        else if (text[i] != '\0' && text[i] != '\n')
+        {
+            return 0;
+        }
+    }
+    *ID = output;
+    return 1;
+}
+
+int validMenuChoice(int maxChoice)
+{
+    char c;
+    while (1)
+    {
+        c = getchar();
+        if (c >= '1' && c <= '9')
+        {
+            if (c-'0' >= 1 && c-'0' <= maxChoice)
+                return c-'0';
+            else
+                return 0;
+        } else return 0;
+    }
+    return -1;
+}
+
+void delayMessage(char msg[])
+{
+    //Adds a delay by asking to click any button before clearing the screen
+    //Used to display an error message or a 'click any button to continue'
+    printf("%s\n", msg);
+    int c = getch();
+    fflush(stdin);
+}
+
 int main()
 {
     StudentList ls;
     student s;
-    createList(&ls);
-    strcpy(s.name, "Name1");
-    s.classID = 1;
-    append(&ls, s);
-    strcpy(s.name, "Name6");
-    s.classID = 1;
-    append(&ls, s);
-    strcpy(s.name, "Name3");
-    s.classID = 1;
-    append(&ls, s);
-    strcpy(s.name, "Name6");
-    s.classID = 3;
-    append(&ls, s);
-    strcpy(s.name, "Name5");
-    s.classID = 2;
-    append(&ls, s);
-    strcpy(s.name, "Name6");
-    s.classID = 7;
-    append(&ls, s);
-    DeleteFromClass(&ls,1);
+    while(1)
+    {
+        system("cls");
+        printf("\tStudent Academic Records System\n");
+        printf("1- Add Student\n");
+        printf("2- Delete Student (by ID or Name)\n");
+        printf("3- Edit Student\n");
+        printf("4- Search Student (by ID or Name)\n");
+        printf("5- Show all Students in a class (Sequentially or in Reverse)\n");
+        printf("6- Delete all Students in a class\n");
+        printf("7- Exit\n\n");
+        printf("Enter your selection: ");
+        switch(validMenuChoice(7))
+        {
+            case 1:
+
+            break;
+            case 2:
+
+            break;
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+            case 5:
+
+            break;
+            case 6:
+
+            break;
+            case 7:
+                return 0;
+            break;
+            default:
+            delayMessage("Error: enter a valid input");
+        }
+    }
     return 0;
 }
